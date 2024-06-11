7ce62a4d5abc3faba6a204a66bd9b3dc0ad2d683
prince981620 <prince981620@gmail.com>
2024-06-11 16:59:23 +0530

    updated index.js

--- todoapp/backend/index.js
+++ todoapp/backend/index.js
@@ -1,14 +1,35 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 app.use(express.json());
 
 app.post("/todo",(req,res)=>{
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg: "You have sent the wrong inputs"
+        })
+        return;
+    }else{ //put in database
 
+    }
 })
 
 app.get("/todos",(req,res)=>{})
 
-app.put("/completed",(req,res)=>{})
+app.put("/completed",(req,res)=>{
+    const updatePayload = req.body;
+    const parseUpPayload = updateTodo.safeParse(updatePayload);
+    if(!parseUpPayload.success){
+        res.status(411).json({
+            msg: "You have sent the wrong id"
+        })
+        return;
+    }else{ //update todo to completed in database
+
+    }
+})
 
 app.listen(3000,()=>{
     console.log("listening on port 3000")
