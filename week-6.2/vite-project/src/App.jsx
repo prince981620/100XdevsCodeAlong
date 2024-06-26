1c5911ae3261fc7c97eba61c23ba9bc251ced3b0
prince981620 <prince981620@gmail.com>
2024-06-26 22:10:04 +0530

    added useCallback intro in app.jsx

--- week-6.2/vite-project/src/App.jsx
+++ week-6.2/vite-project/src/App.jsx
@@ -1,29 +1,31 @@
-import { useMemo, useState } from "react"
+import { memo, useCallback, useMemo, useState } from "react"
 
 function App(){
-  const [limit,setLimit]=useState(0);
-  const [count,setCount] = useState(1);
-  // function calculateSum(){
-    // setCount(count+1);
-    // const limit = document.getElementById("input").value;
+  const [count,setCount] = useState(0);
 
-    // usinf useMemo to prevent this expensive task to rerun on every rerendrer
-    const sum = useMemo(()=>{
-      console.log("this expensive funciton got called");
-      let temp = 0;
-      for(let i=1;i<=limit;i++){
-        temp += i;
-      }
-      return temp;
-    },[limit]) // now clilking on counter button rerender the App but this expensive operation runs
-              //  only when the depedency array limit changes
+  // function inputFun(){
+  //   console.log("Hi!");
   // }
+
+  const inputFun = useCallback(()=>{
+    console.log("Hi!");
+  },[]) // now this function wont change or redefined on each rerender so the component inside
+  //      memo wont rerenedered on each render due to button setCount
+
   return <div>
-    {/* <input id="input" onChange={calculateSum} /> */}
-    <input onChange={(e)=>{setLimit(e.target.value)}} />
-    <div>Sum from 1 to {limit} is {sum}</div>
+    <ButtonComponent inputFunction={inputFun}/>
     <button onClick={()=>{setCount(count+1)}} >Counter ({count})</button>
   </div>
 }
+// even when the component ButtonComponent is wrapped inside memo this is getting re-renderd
+// when clicking button of setCount because react is dumb it thiks the inputFun was redefined when 
+// setCount rerenderd the APP
+// soln -> wrap inputFun in useCallback -> used to memoise a funcition
+const ButtonComponent = memo(({inputFun})=>{
+  console.log("child rendered");
+  return <div>
+    <button> Button Clicked</button>
+  </div>
+})
 
 export default App;
