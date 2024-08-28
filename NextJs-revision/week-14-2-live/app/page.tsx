f56152537461e291e043f0d197be2dc8985d0360
prince981620 <prince981620@gmail.com>
2024-08-28 07:39:24 +0530

    next backend revision

---
+++ NextJs-revision/week-14-2-live/app/page.tsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import Image from "next/image";
+
+const  getdata = async ()=>{
+  const response = await axios.get("http://localhost:3001/api/user")
+  return response.data;
+}
+
+export default async function Home() {
+  const userData = await getdata();
+  return (
+<div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>  
+);}
